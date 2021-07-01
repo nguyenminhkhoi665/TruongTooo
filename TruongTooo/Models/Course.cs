d68beefd5b67ea97a073382cfce8a5b6215d5491
MinhKhoi810 <khoi1221224@gmail.com>
2021-07-01 21:01:52 +0700

    Add project files.

---
+++ TruongTooo/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TruongTooo.Models
+{
+    public class Course
+    {
+      
+            public int Id{ get; set; }
+
+            public ApplicationUser Lecture { get; set; }
+            [Required]
+            public string LectureId { get; set; }
+            [Required]
+            [StringLength(250)]
+            public string Place { get; set; }
+            public DateTime DateTime { get; set; }
+
+            public Category Category { get; set; }
+            [Required]
+            public byte CategoryId { get; set; }
+        }
+
+
+}
